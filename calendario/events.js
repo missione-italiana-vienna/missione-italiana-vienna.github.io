ccbf75468c7171bdae9329311093b61fb7f4d82b
Matteo Tommasini <matteo.tommasini2@gmail.com>
2019-05-30 10:11:38 +0200

    finished porting the events of the calendar from html to js

---
+++ calendario/events.js
@@ -0,0 +1,291 @@
+var events = 
+[
+	{
+		'date': '30 Maggio 2019',
+		'content': 
+		[ 
+			{
+				'description': '<b>Ascensione</b>'
+			},
+			{
+				'style': 'color: red;',
+				'time': '11:30',
+				'description': 'La Messa di oggi è posticipata alle <b>11:30</b> per non sovrapporci alla celebrazione delle Prime Comunioni della parrocchia di lingua tedesca.'
+			}
+		]
+	},
+	{
+		'date': '1 Giugno 2019',
+		'content':
+		[
+			{
+				'time': '14:30 - 16:30 (*)',
+				'description': 'Confessioni.'
+			},
+			{
+				'time': '16:30 (*)',
+				'description': 'recita del Rosario.'
+			},
+			{
+				'time': '16:30 (*)',
+				'description': 'Messa prefestiva.'
+			},
+			{
+				'type': 'comment',
+				'description': '(*) = nella cappella di Sant\'Antonio, entrata dal numero civico 17 di fianco alla Chiesa. Se avete bisogno di assistenza con passeggini o sedie a rotelle, contattate per favore in anticipo padre Thomas.'
+			}
+		]
+	},
+	{
+		'date': '2 Giugno 2019',
+		'content': 
+		[
+			{
+				'time': '10:30',
+				'description': 'S. Messa'
+			}
+		]
+	},
+	{
+		'date': '5 Giugno 2019',
+		'content': 
+		[
+			{
+				'time': '19:00 - 20:00 (*)',
+				'description': 'Adorazione Eucaristica in silenzio.'
+			},
+			{
+				'type': 'comment',
+				'description': '(*) = nella cappella di Sant\'Antonio, entrata dal numero civico 17 di fianco alla Chiesa. Se avete bisogno di assistenza con passeggini o sedie a rotelle, contattate per favore in anticipo padre Thomas.'
+			}
+		]
+	},
+	{
+		'date': '7 Giugno 2019',
+		'content': 
+		[
+			{
+				'time': 'dalle 19:30 (*)',
+				'description': 'Adorazione Eucaristica notturna in silenzio (fino alle 7:00 del mattino dopo). L\'Adorazione si svolge nella <b>Cappella di Sant\'Antonio</b> (entrata dal civico di Alser Straße 17, di fianco alla chiesa) (*). Siete invitati a vegliare in preghiera quanto tempo volete e riuscite. In particolare, <b>dalle 21:00 alle 22:00</b> l'Adorazione è animata dai frati con canti di preghiera.'
+			},
+			{
+				'type': 'comment',
+				'description': '(*) = se avete bisogno di assistenza con passeggini o sedie a rotelle, contattate per favore in anticipo padre Thomas.'
+			}
+		]
+	},
+	{
+		'date': '8 Giugno 2019',
+		'content': 
+		[
+			{
+				'time': '14:30 - 16:30 (*)',
+				'description': 'Confessioni.'
+			},
+			{
+				'time': '16:30 (*)',
+				'description': 'recita del Rosario.'
+			},
+			{
+				'time': '17:00 (*)',
+				'description': 'Messa prefestiva.'
+			},
+			{
+				'type': 'comment',
+				'description': '(*) = nella cappella di Sant\'Antonio, entrata dal numero civico 17 di fianco alla Chiesa. Se avete bisogno di assistenza con passeggini o sedie a rotelle, contattate per favore in anticipo padre Thomas.'
+			}
+		]
+	},
+	{
+		'date': '9 Giugno 2019',
+		'content':
+		[
+			{
+				'time': '10:30',
+				'description': 'S. Messa.'
+			},
+			{
+				'description': 'In questa data non ci sarà nessuna messa prefestiva per Pentecoste.'
+			}
+		]
+	},
+	{
+		'date': '10 Giugno 2019',
+		'content': 
+		[
+			{
+				'description': '<b>Lunedì di Pentecoste</b>'
+			},
+			{
+				'time': '10:30',
+				'description': 'S. Messa.'
+			}
+		]
+	},
+	{
+		'date': '15 Giugno 2019',
+		'content': 
+		[
+			{
+				'time': '14:30 - 16:30 (*)',
+				'description': 'Confessioni.'
+			},
+			{
+				'time': '16:30 (*)',
+				'description': 'recita del Rosario.'
+			},
+			{
+				'time': '17:00 (*)',
+				'description': 'Messa prefestiva'
+			},
+			{
+				'type': 'comment',
+				'description': '(*) = nella cappella di Sant\'Antonio, entrata dal numero civico 17 di fianco alla Chiesa. Se avete bisogno di assistenza con passeggini o sedie a rotelle, contattate per favore in anticipo padre Thomas.'
+			}
+		]
+	},
+	{
+		'date': '16 Giugno 2019',
+		'content':
+		[
+			{
+				'description': 'Festa parrocchiale in occasione della SS. Trinità. Questa Domenica non ci sarà una Messa in italiano, ma una celebrazione unica assieme alla parrocchia di Alserstrasse in tedesco (con alcune parti della Messa in italiano). Per seguire la liturgia interamente in italiano si consiglia di andare alla Messa prefestiva di Sabato 15 Giugno. La celebrazione sarà seguita da una festa parrocchiale (Luftburg per bambini, cibo, etc.). Contattate padre Thomas se volete dare una mano con l'organizzazione.'
+			}
+		]
+	},
+	{
+		'date': '19 Giugno 2019',
+		'content':
+		[
+			{
+				'time': '19:00 - 20:00 (*)',
+				'description': 'Adorazione Eucaristica in silenzio.'
+			},
+			{
+				'type': 'comment',
+				'description': '(*) = nella cappella di Sant\'Antonio, entrata dal numero civico 17 di fianco alla Chiesa. Se avete bisogno di assistenza con passeggini o sedie a rotelle, contattate per favore in anticipo padre Thomas.'
+			}
+		]
+	},
+	{
+		'date': '20 Giugno 2019',
+		'content':
+		[
+			{
+				'description': '<b>Corpus Domini</b>'
+			},
+			{
+				'description': 'Processione che terminerà alla chiesa di Alser Straße (orario ancora da stabilire). In questa data non ci sarà la Messa in italiano. Contattate padre Thomas se volete dare una mano con l'organizzazione della processione.'
+			}
+		]
+	},
+	{
+		'date': '22 Giugno 2019',
+		'content':
+		[
+			{
+				'time': '14:30 - 16:30 (*)',
+				'description': 'Confessioni.'
+			},
+			{
+				'time': '16:30 (*)',
+				'description': 'recita del Rosario.'
+			},
+			{
+				'time': '17:00 (*)',
+				'description': 'Messa prefestiva.'
+			},
+			{
+				'type': 'comment',
+				'description': '(*) = nella cappella di Sant\'Antonio, entrata dal numero civico 17 di fianco alla Chiesa. Se avete bisogno di assistenza con passeggini o sedie a rotelle, contattate per favore in anticipo padre Thomas.'
+			}
+		]
+	},
+	{
+		'date': '23 Giugno 2019',
+		'content': 
+		[
+			{
+				'time': '10:30',
+				'description': 'S. Messa.'
+			}
+		]
+	},
+	{
+		'date': '24 Giugno 2019',
+		'content':
+		[
+			{
+				'time': 'dalle 19:00',
+				'description': '<a class = "normal_link" href = "https://mcivienna.org/attivita/#lectio_divina">Lectio Divina</a> per i giovani e giovani adulti (entrata da Alser Straße 17).'
+			}
+		]
+	}
+];
+
+
+// Do not modify the next lines!
+
+var appointments = "";
+
+for (var i = 0; i < events.length; ++i) {
+	var content = "";
+
+	// compute the content of the event
+	for (var j = 0; j < events[i].content.length; ++j) {
+		content += '<tr><td';
+
+		if (events[i].content[j].hasOwnProperty("style")) {
+			content += ' style = "' + events[i].content[j].style + '"';
+		}
+
+		if (events[i].content[j].hasOwnProperty("time")) {
+			content += ' class = "time_event"';
+		}
+		else {
+			content += ' class = "time_event_empty"';
+		}
+		content += '>'; // this closes '<td' inserted above
+		if (events[i].content[j].hasOwnProperty("time")) {
+			content += events[i].content[j].time;
+		}
+		
+		content += '</td>' // this closes '<td ... >' already inserted above
+		content += '<td class = "description_event';
+		if (events[i].content[j].hasOwnProperty("type") && events[i].content[j].type == "comment") {
+			content += ' event_comment';
+		}
+		content += '">' + events[i].content[j].description + '</td>'
+		content += '</tr>'; // this closes '<tr>' already inserted above
+	}
+
+	var splitted_date = events[i].date.split(" ");
+	var day = splitted_date[0];
+	var month = splitted_date[1]; // this is a string of the form "Gennaio", etc (see below)
+	var year = splitted_date[2];
+
+	var list_months = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
+			
+	// Note: the next value will be a number between 0 and 11, thus compatible with the way JS handles months (0 = January for Javascript)
+
+	// see https://www.w3schools.com/jsref/jsref_findindex.asp 
+	// (the method findIndex() requires as input value a function returning boolean results)
+	function FindMonthByName(name_month) {
+    return name_month == this;
+	}
+  var month_number = list_months.findIndex(FindMonthByName, month);
+
+  // starting with the infos about year, month number and date, we create a new date object
+	var date = new Date(year, month_number, day);
+
+	// we extract the day of the week from the date object
+	var list_days = ["Domenica", "Lunedì", "Martedì", "Mercoledì", "Giovedì", "Venerdì", "Sabato"];
+	var day_name = list_days[date.getDay()];
+
+	// merges the content of the event with the additional infos about the date
+  appointments += '<tr class = "events_on_given_day"><td class = "date_event"><div>' + day_name + '</div><div>'+ day + '</div><div>' + month + '</div></td><td class = "text_event"><table>' + content + '</table></td></tr>';
+}
+
+// attach the content of appointments to the appointments_container already defined in the base HTML code
+var appointments_container = document.getElementById("appointments_container");
+appointments_container.innerHTML = appointments;
