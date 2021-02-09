8e2044e3e97950397ea8b0c5440fd3c17ce39bb6
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2021-02-09 17:20:50 +0100

    testing with the format of the json file

--- calendario/eventi.js
+++ calendario/eventi.js
@@ -1,16 +1,6 @@
 [
   {
     "date": "7 Febbraio 2021",
-    "content": 
-    [
-      {
-        "time": "11:30",
-        "description": "S. Messa."
-      },
-      {
-        "time": "16:00",
-        "description": "S. Messa (solo questa Domenica abbiamo questa S. Messa la domenica pomeriggio; dal prossimo weekend si riprende con gli orari normali)."
-      }
-    ]
+    "content": "test"
   }
 ]
