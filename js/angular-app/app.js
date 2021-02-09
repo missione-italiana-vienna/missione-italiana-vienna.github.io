6f363a7334d342627473586698c18a4686183218
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2021-02-09 16:08:50 +0100

    first step for moving the calendar page to the common template

--- js/angular-app/app.js
+++ js/angular-app/app.js
@@ -250,6 +250,13 @@ function($scope, $rootScope, $route, sharedProperties) {
 
     // here we must insert the routines for the streaming and for the calendar, again 
     // using the variable type_of_controller
+
+    if (type_of_controller === "calendario") {
+
+
+      
+      sharedProperties.generate_html_with_all_events("future");
+    }
   }
 
 
@@ -264,6 +271,14 @@ function($rootScope, $sce, $q, $httpParamSerializerJQLike) {
   var type_of_controller = "";
 
   var popups_already_created = false;
+
+  // 2 global arrays, used below
+  var list_months = ["Gennaio", "Febbraio", "Marzo", "Aprile", "Maggio", "Giugno", 
+    "Luglio", "Agosto", "Settembre", "Ottobre", "Novembre", "Dicembre"];
+
+  var list_weekdays = ["Domenica", "Lunedì", "Martedì", 
+    "Mercoledì", "Giovedì", "Venerdì", "Sabato"];
+
 
   function set_popup_fontsize() {
     var viewport_width = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
@@ -296,6 +311,61 @@ function($rootScope, $sce, $q, $httpParamSerializerJQLike) {
       }
       popup_notification_buttons[i].style.height = height_button + "px"; 
     }
+  };
+
+  function generate_html_for_a_given_date(js_content, day, month, weekday) {
+    var content = "";
+
+    // compute the content of the event
+    for (var j = 0; j < js_content.length; ++j) {
+      content += '<tr><td';
+
+      if (js_content[j].hasOwnProperty("style")) {
+        content += ' style = "' + js_content[j].style + '"';
+      }
+
+      if (js_content[j].hasOwnProperty("time")) {
+        content += ' class = "time_event"';
+      }
+      else {
+        content += ' class = "time_event_empty"';
+      }
+      content += '>'; // this closes '<td' inserted above
+      if (js_content[j].hasOwnProperty("time")) {
+        content += js_content[j].time;
+      }
+      
+      content += '</td> ' // this closes '<td ... >' already inserted above
+      // NOTE: in the string above there's a white space; this is needed for the visualization on
+      // small screens, where every td is displayed with "display: inline;"
+
+      content += '<td class = "description_event';
+      if (js_content[j].hasOwnProperty("type") && js_content[j].type == "comment") {
+        content += ' event_comment';
+      }
+      content += '">' + js_content[j].description + '</td>'
+      content += '</tr>'; // this closes '<tr>' already inserted above
+    }
+
+    // Merges the content of the event with the additional infos about the date
+    // NOTE: do not remove the white spaces in the strings below: they are needed for the visualization on
+    // small screens, where every tr, td and div is displayed with "display: inline;" 
+    return ' <tr class = "events_on_given_day">' + 
+              '<td class = "date_event">' +
+                '<div>' + weekday + '</div> '+
+                '<div>' + day + '</div> '+
+                '<div>' + month + '</div>' +
+              '</td>' +
+              '<td class = "text_event">' +
+                '<table>' + content + '</table>'+
+              '</td>' + 
+            '</tr>';
+  };
+
+  // see https://www.w3schools.com/jsref/jsref_findindex.asp 
+  // The method findIndex() requires as input value a function returning boolean results:
+  function FindMonthByName(name_month) {
+    return name_month == this;
   };
 
   return {
@@ -408,7 +478,6 @@ function($rootScope, $sce, $q, $httpParamSerializerJQLike) {
     },
 
     
-  
     create_popup_links: function(popups) {
 
       function create_single_popup_link(text, link) {
@@ -460,6 +529,86 @@ function($rootScope, $sce, $q, $httpParamSerializerJQLike) {
   
         var name_of_file = pad(i, 3);
         $("#container_of_news_" + i).html("Loading...").load("https://mcivienna.org/home/" + name_of_file + ".html");
+      }
+    },
+
+    generate_html_with_all_events: function(input_string) {
+      if (input_string != "future" && input_string != "past") {
+        console.log('Wrong input parameter: must be either "future" or "past"');
+      }
+      else {
+        var appointments = "";
+        var i, splitted_date, day, month, year, month_number, date_considered, comparison_date, weekday, current_string_date;
+
+        function ($scope, $http) {
+
+          var request = {
+              method: 'get',
+              url: 'https://mcivienna.org/calendario/eventi.json',
+              dataType: 'json',
+              contentType: "application/json"
+          };
+
+          $http(request)
+              .success(function (jsonData) {
+                  var events = jsonData;
+
+                  // the next code must be included here actually
+              })
+              .error(function () {
+                console.log("Unable to parse the events from the local file!");
+              });
+
+
+        for (i = 0; i < events.length; ++i) {
+
+          // converts the date from the format "10 Gennaio 2019" to the format year=2019, month=0 (months start with 0 in JS), day=10
+          current_string_date = events[i].date.trim(); // trims white spaces at the beginning and at the end of the string
+          current_string_date = current_string_date.replace(/\s\s+/g, ' '); // replaces (multiple) spaces, tabs etc with single spaces
+          splitted_date = current_string_date.split(" "); // splits using single spaces as delimiters
+          day = splitted_date[0];
+          month = splitted_date[1]; // this is a string of the form "Gennaio", etc (see below)
+          year = splitted_date[2];
+              
+          // Note: the next value will be a number between 0 and 11, thus compatible with the way JS handles months (0 = January for Javascript)
+
+          month_number = list_months.findIndex(FindMonthByName, month);
+
+          if (month_number == -1) {
+            console.log('Error! Month not found in the list: ' + month);
+            console.log(events[i]);
+          }
+
+          // starting with the infos about year, month number and date, we create a new date object
+          date_considered = new Date(year, month_number, day);
+
+          // we extract the day of the week from the date object
+          weekday = list_weekdays[date_considered.getDay()];
+
+          // we set a comparison date
+          comparison_date = new Date();  // currently it's equal to today, it will be modified below
+
+          if (input_string == "future") {
+            comparison_date.setDate(comparison_date.getDate() - 1); // the comparison date is now equal to yesterday
+
+            // we consider only those events that are future events, current events, or events not older that yesterday
+            if (date_considered >= comparison_date) {			  
+              appointments += generate_html_for_a_given_date(events[i].content, day, month, weekday);
+            }
+          }
+          else { // i.e. if input_string == "past"
+            // the comparison date is not modified in this case
+
+            // we consider only those events that are past events
+            if (date_considered < comparison_date) {
+              // we add events in a reverse order
+              appointments = generate_html_for_a_given_date(events[i].content, day, month, weekday) + appointments;
+            }
+          }
+        }
+        // attach the content of appointments to the appointments_container already defined in the base HTML code
+        var appointments_container = document.getElementById("appointments_container");
+        appointments_container.innerHTML = appointments;
       }
     }
 
