341365bc22d132cef1660a11c9c77bf6bd5530e3
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2021-03-22 10:33:50 +0100

    added a new blog entry with the dates of the Settimana Santa, changed the popup in the homepage so that it points to this blog entry

--- home/popups.js
+++ home/popups.js
@@ -1,10 +1,6 @@
 [
   {
-    "text": "Informazioni per i nuovi corsi di preparazione al matrimonio e alla cresima per adulti",
-    "link": "https://mcivienna.org/#!/blog/2021/01/23/corsi_matrimonio_e_cresima/"
-  },
-  {
-    "text": "Ripresa delle celebrazioni con l'assemblea",
-    "link": "https://mcivienna.org/#!/blog/2021/02/05/ripresa_delle_celebrazioni_con_assemblea/"
+    "text": "Orari delle celebrazioni della Settimana Santa",
+    "link": "https://mcivienna.org/#!/blog/2021/03/22/celebrazioni_della_settimana_santa/"
   }
 ]
