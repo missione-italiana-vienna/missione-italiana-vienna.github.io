8c304addf81e14d12fae273a0103d51f39e4ae94
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2021-04-11 19:16:13 +0200

    removed the current popup, added a template for future use

--- home/popups.js
+++
@@ -1,6 +0,0 @@
-[
-  {
-    "text": "Orari delle celebrazioni della Settimana Santa",
-    "link": "https://mcivienna.org/#!/blog/2021/03/22/celebrazioni_della_settimana_santa/"
-  }
-]
---
+++ home/popups_TEMPLATE.js
@@ -0,0 +1,11 @@
+// IN ORDER TO HAVE A VALID POPUP:
+// 1) copy this template to a file (in this same folder) named popups.js
+// 2) remove all these comments
+// 3) in case you want to add multiple buttons in the popup, each must be provided
+//    as content between {...}. Each block {...} must be separated by commas from the next one!
+[
+  {
+    "text": "Orari delle celebrazioni ...",
+    "link": "https://mcivienna.org/..."
+  }
+]
