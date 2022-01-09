a5e8a4918ab797d134f300f473a410a7b5f6b926
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2022-01-09 10:42:43 +0100

    updated a link

--- calendario/eventi.js
+++ calendario/eventi.js
@@ -683,7 +683,7 @@
     [
       {
         "time": "19:00",
-        "description": "<a href = \"https://mcivienna.org/#!/attivita/\">Lectio Divina</a> per giovani e giovani adulti (<a href = \"https://mcivienna.org/#!/attivita/lectio_divina/2022-05-15/\">testo qui</a>). Entrata da Alser Straße 17."
+        "description": "<a href = \"https://mcivienna.org/#!/attivita/\">Lectio Divina</a> per giovani e giovani adulti (<a href = \"https://mcivienna.org/#!/attivita/lectio_divina/2022-06-15/\">testo qui</a>). Entrata da Alser Straße 17."
       }
     ]
   },
