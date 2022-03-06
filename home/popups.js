ccc5cac5d6b9fd8f01dd1286557fe0f5a34b0499
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2022-03-07 00:35:56 +0100

    added blog entry

--- home/popups.js
+++ home/popups.js
@@ -1,10 +1,6 @@
 [
   {
-    "text": "Preghiera e donazioni per la popolazione ucraina",
-    "link": "https://mcivienna.org/#!/blog/2022/03/01/preghiera_per_ucraina/"
+    "text": "Preghiera e donazioni per la popolazione ucraina - IBAN per donazioni",
+    "link": "https://mcivienna.org/#!/blog/2022/03/07/donazioni_per_ucraina/"
   },
-  {
-    "text": "Mercoledì delle Ceneri (2 Marzo): celebrazione alle ore 19:30",
-    "link": "https://mcivienna.org/#!/calendario/"
-  }
 ]
