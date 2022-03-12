f33745964eb54472f51c7d8dda752a4c0c7e37e7
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2022-03-12 13:54:26 +0100

    updated the popup

--- home/popups.js
+++ home/popups.js
@@ -1,6 +1,6 @@
 [
   {
-    "text": "Preghiera e donazioni per la popolazione ucraina",
+    "text": "Preghiera e donazioni per la popolazione ucraina - resoconto sulle donazioni",
     "link": "https://mcivienna.org/#!/blog/2022/03/12/donazioni_per_ucraina/"
   }
 ]
