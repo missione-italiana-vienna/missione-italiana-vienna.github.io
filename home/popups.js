40059d31af6f5b68100eec8b1d07e42b47776f3b
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2022-03-14 11:37:45 +0100

    fixed the popup

--- home/popups.js
+++ home/popups.js
@@ -4,7 +4,7 @@
     "link": "https://mcivienna.org/#!/blog/2022/03/12/donazioni_per_ucraina/"
   },
   {
-    "text": "Elezioni del consiglio pastorale",
+    "text": "Elezioni del consiglio pastorale parrocchiale",
     "link": "https://mcivienna.org/#!/blog/2022/03/14/elezioni_consiglio_pastorale/"
   }
 ]
