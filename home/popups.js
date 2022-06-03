6d55afba74caa87a34a83d92b0e72ee683e08ee8
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2022-06-03 13:37:38 +0200

    changed the infos in popups and calendar

--- home/popups.js
+++ home/popups.js
@@ -1,10 +1,6 @@
 [
   {
-    "text": "Preghiera e donazioni per la popolazione ucraina - resoconto sulle donazioni",
-    "link": "https://mcivienna.org/#!/blog/2022/03/12/donazioni_per_ucraina/"
-  },
-  {
-    "text": "Risultati delle elezioni del consiglio pastorale parrocchiale",
-    "link": "https://mcivienna.org/#!/blog/2022/04/26/elezioni_consiglio_pastorale/"
+    "text": "Messaggio per Pentecoste",
+    "link": "https://mcivienna.org/#!/blog/2022/06/03/messaggio_per_pentecoste/"
   }
 ]
