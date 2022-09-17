fa2a251eb9d880d831e65542955d903aa10df9ef
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2022-09-17 10:50:38 +0200

    changed the popup in the homepage

--- home/popups.js
+++ home/popups.js
@@ -1,6 +1,6 @@
 [
   {
-    "text": "Messaggio per Pentecoste",
-    "link": "https://mcivienna.org/#!/blog/2022/06/03/messaggio_per_pentecoste/"
+    "text": "Messaggio per la ripresa dell'attività pastorale e per la festa con la parrocchia di Alserstraße",
+    "link": "https://mcivienna.org/#!/blog/2022/09/17/ripresa_attivita_pastorale_e_festa_della_parrocchia/"
   }
 ]
