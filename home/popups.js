63ee9dc4ff450ada1164275fed5eec99fb69c488
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2022-11-05 14:41:02 +0100

    added popup for the next Agape

--- home/popups.js
+++ home/popups.js
@@ -2,5 +2,9 @@
   {
     "text": "Corso per adulti per la preparazione al Sacramento della Cresima - iscrizioni aperte fino al 7 Novembre",
     "link": "https://mcivienna.org/#!/blog/2022/10/24/corso_per_adulti_in_preparazione_alla_cresima/"
+  },
+  {
+    "text": "La prossima Agape della comunità si terrà Domenica 27 Novembre dopo la S. Messa delle 11:30",
+    "link": "https://mcivienna.org/#!/calendario/"
   }
 ]
