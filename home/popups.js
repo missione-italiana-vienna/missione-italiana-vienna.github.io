6db2a49a8d01b3e39bdda7f693f350a7ba898ab4
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2022-11-25 09:11:08 +0100

    changed the popup with the new message

--- home/popups.js
+++ home/popups.js
@@ -1,7 +1,7 @@
 [
   {
-    "text": "Corso per adulti per la preparazione al Sacramento della Cresima - iscrizioni aperte fino al 7 Novembre",
-    "link": "https://mcivienna.org/#!/blog/2022/10/24/corso_per_adulti_in_preparazione_alla_cresima/"
+    "text": "Messaggio per l'inizio dell'Avvento",
+    "link": "https://mcivienna.org/#!/blog/2022/11/24/messaggio_per_avvento_2022/"
   },
   {
     "text": "La prossima Agape della comunità si terrà Domenica 27 Novembre dopo la S. Messa delle 11:30",
