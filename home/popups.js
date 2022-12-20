dae21a58756388e540af43a1cd2c466f6c8a6692
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2022-12-20 09:41:44 +0100

    changed the expiry date of a popup

--- home/popups.js
+++ home/popups.js
@@ -12,6 +12,6 @@
   {
     "text": "Messaggio per la ripresa delle attività del coro",
     "link": "https://mcivienna.org/#!/blog/2022/12/20/messaggio_per_la_ripresa_delle_attivita_del_coro/",
-    "last_date_to_show": "2022-12-31"
+    "last_date_to_show": "2023-01-09"
   }
 ]
