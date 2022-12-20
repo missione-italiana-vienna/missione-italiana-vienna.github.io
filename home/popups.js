6b0622d7e9266149b7c814fd09798502fbecf50c
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2022-12-20 09:39:09 +0100

    added message for the chorus, changed the routine for popups now incorporating a last date to be shown

--- home/popups.js
+++ home/popups.js
@@ -1,10 +1,17 @@
 [
   {
     "text": "Messaggio per l'inizio dell'Avvento",
-    "link": "https://mcivienna.org/#!/blog/2022/11/24/messaggio_per_avvento_2022/"
+    "link": "https://mcivienna.org/#!/blog/2022/11/24/messaggio_per_avvento_2022/",
+    "last_date_to_show": "2022-12-24"
   },
   {
     "text": "La prossima Agape della comunità si terrà Domenica 27 Novembre dopo la S. Messa delle 11:30",
-    "link": "https://mcivienna.org/#!/calendario/"
+    "link": "https://mcivienna.org/#!/calendario/",
+    "last_date_to_show": "2022-11-27"
+  },
+  {
+    "text": "Messaggio per la ripresa delle attività del coro",
+    "link": "https://mcivienna.org/#!/blog/2022/12/20/messaggio_per_la_ripresa_delle_attivita_del_coro/",
+    "last_date_to_show": "2022-12-31"
   }
 ]
--- home/popups_TEMPLATE.js
+++ home/popups_TEMPLATE.js
@@ -4,9 +4,15 @@
 // 3) in case you want to add multiple buttons in the popup, each must be provided
 //    as content between {...}. Each block {...} must be separated by commas from the next one!
 // 4) if you do not want any popup, the file must consist of only an empty array
+// NOTE: the object "last_date_to_show" must contain a string date in international format, namely yyyy-mm-dd
+// This is the last date when the corresponding popup will be shown on the website.
+// In this way we do not need to immediately manually update the popup file when a popup is no more relevant
+// but we can decide already in advance when the popup shall disappear from the website
+// NOTE: if you do not know yet how long the popup will have to be shown, put a date very far in the future
 [
   {
     "text": "Orari delle celebrazioni ...",
-    "link": "https://mcivienna.org/..."
+    "link": "https://mcivienna.org/...",
+    "last_date_to_show": "2022-12-20"
   }
 ]
