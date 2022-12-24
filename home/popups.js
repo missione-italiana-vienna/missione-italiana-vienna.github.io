55dafbe2b80c13d6ed7ab43be8a731a528a3613d
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2022-12-24 13:24:40 +0100

    added new message for Christmas 2022

--- home/popups.js
+++ home/popups.js
@@ -1,13 +1,8 @@
 [
   {
-    "text": "Messaggio per l'inizio dell'Avvento",
-    "link": "https://mcivienna.org/#!/blog/2022/11/24/messaggio_per_avvento_2022/",
-    "last_date_to_show": "2022-12-24"
-  },
-  {
-    "text": "La prossima Agape della comunità si terrà Domenica 27 Novembre dopo la S. Messa delle 11:30",
-    "link": "https://mcivienna.org/#!/calendario/",
-    "last_date_to_show": "2022-11-27"
+    "text": "Messaggio per Natale 2022 e per l'anno nuovo",
+    "link": "https://mcivienna.org/#!/blog/2022/12/24/messaggio_per_natale_2022/",
+    "last_date_to_show": "2023-01-06"
   },
   {
     "text": "Messaggio per la ripresa delle attività del coro",
