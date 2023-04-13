d4284af98276c3043222639cef378bdaf75aada3
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2023-04-13 11:22:19 +0200

    added next appointment of the Agape in the homepage (as popup), list of activities and calendar

--- home/popups.js
+++ home/popups.js
@@ -3,5 +3,10 @@
     "text": "Messaggio per Pasqua",
     "link": "https://mcivienna.org/#!/blog/2023/04/08/messaggio_per_pasqua_2023/",
     "last_date_to_show": "2023-05-09"
+  },
+  {
+    "text": "Prossima Agape: Domenica 23 Aprile",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "last_date_to_show": "2023-04-23"
   }
 ]
