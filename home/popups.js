1776bca6544947cfa7595786a7eded4507aa9f23
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2023-05-26 22:15:21 +0200

    added last blogpost to the popups

--- home/popups.js
+++ home/popups.js
@@ -1,5 +1,10 @@
 [
   {
+    "text": "Messaggio per Pentecoste",
+    "link": "https://mcivienna.org/#!/blog/2023/05/26/messaggio_per_pentecoste_2023/",
+    "last_date_to_show": "2023-06-25"
+  },
+  {
     "text": "Festa della comunità italiana: lunedì di Pentecoste (29 Maggio)",
     "link": "https://mcivienna.org/#!/calendario/",
     "last_date_to_show": "2023-05-29"
