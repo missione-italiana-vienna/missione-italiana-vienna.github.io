b737d66729eb4cd0d9c999d4e542a978fea5054d
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2023-05-27 07:57:28 +0200

    added popup

--- home/popups.js
+++ home/popups.js
@@ -13,5 +13,10 @@
     "text": "Lunga Notte delle Chiese: Venerdì 2 Giugno",
     "link": "https://mcivienna.org/#!/calendario/",
     "last_date_to_show": "2023-06-02"
-  }
+  },
+  {
+    "text": "Domenica 4 Giugno: S. Messa (in italiano e tedesco) anticipata alle ore 11:00",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "last_date_to_show": "2023-06-04"
+  },
 ]
