adf6289fd956dfdea2072c877f1c1ae7be5f888b
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2023-06-01 11:21:07 +0200

    fixed a bug in the json file

--- calendario/eventi.js
+++ calendario/eventi.js
@@ -887,7 +887,7 @@
       {
         "time": "11:00",
         "style": "color: red;",
-        "description": "S. Messa (in italiano e tedesco) insieme alla comunità parrocchiale di Alser Strasse (notare il cambio di orario rispetto al solito!). Se volete seguire la S. Messa solo in italiano, consigliamo di andare alla S. Messa prefestiva di sabato 3 Giugno. Dopo la celebrazione Eucaristica di domenica ci sarà una festa nel giardino del convento, oppure nel chiostro in caso di maltempo. Il pranzo durante questa festa sarà organizzato a buffet. Ci saranno piatti che potranno essere acquistati a prezzi modici (cotoletta con crauti, salsiccia con panino, bevande e torte). Vi chiediamo cortesemente di non portare cibo da casa da condividere, in quanto l\'organizzazione non prevede tale modalità."
+        "description": "S. Messa (in italiano e tedesco) insieme alla comunità parrocchiale di Alser Strasse (notare il cambio di orario rispetto al solito!). Se volete seguire la S. Messa solo in italiano, consigliamo di andare alla S. Messa prefestiva di sabato 3 Giugno. Dopo la celebrazione Eucaristica di domenica ci sarà una festa nel giardino del convento, oppure nel chiostro in caso di maltempo. Il pranzo durante questa festa sarà organizzato a buffet. Ci saranno piatti che potranno essere acquistati a prezzi modici (cotoletta con crauti, salsiccia con panino, bevande e torte). Vi chiediamo cortesemente di non portare cibo da casa da condividere, in quanto l'organizzazione non prevede tale modalità."
       }
     ]
   },
