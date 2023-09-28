67380d85ca2ecf62a3ca64be8af6d8f5e7deba16
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2023-09-28 15:58:20 +0200

    added contact detail to the page of the catechismo

--- home/popups.js
+++ home/popups.js
@@ -4,5 +4,12 @@
     "link": "https://mcivienna.org/#!/calendario/",
     "first_date_to_show": "2023-09-27",
     "last_date_to_show": "2023-10-01"
+  },
+  {
+    "text": "Iscrizione al catechismo fino al 10 Ottobre",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2023-09-27",
+    "last_date_to_show": "2023-10-01"
   }
+
 ]
