59aac2620653ca842e5e32d0dc84778989cf41ac
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2023-09-28 16:00:12 +0200

    added popup with message by p. Thomas

--- home/popups.js
+++ home/popups.js
@@ -7,9 +7,14 @@
   },
   {
     "text": "Iscrizione al catechismo fino al 10 Ottobre",
-    "link": "https://mcivienna.org/#!/blog/2023/09/28/catechismo_2023_2024",
+    "link": "https://mcivienna.org/#!/blog/2023/09/28/catechismo_2023_2024/",
     "first_date_to_show": "2023-09-28",
     "last_date_to_show": "2023-10-11"
+  },
+  {
+    "text": "Messaggio per il nuovo anno pastorale",
+    "link": "https://mcivienna.org/#!/blog/2023/09/28/messaggio_per_il_nuovo_anno_pastorale/",
+    "first_date_to_show": "2023-09-28",
+    "last_date_to_show": "2023-10-28"
   }
-
 ]
