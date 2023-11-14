471e4d0e64290ceae20a8dbe75fe0b582f01971b
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2023-11-14 16:45:26 +0100

    added popup in the homepage

--- home/popups.js
+++ home/popups.js
@@ -1,20 +1,8 @@
 [
   {
-    "text": "Domenica 1 Ottobre: S. Messa con la comunità austriaca di Alser Straße anticipata alle ore 11:00, a seguire pranzo in giardino",
+    "text": "Prossima Agape della comunità: 19 Novembre",
     "link": "https://mcivienna.org/#!/calendario/",
-    "first_date_to_show": "2023-09-27",
-    "last_date_to_show": "2023-10-01"
-  },
-  {
-    "text": "Iscrizione al catechismo fino al 10 Ottobre",
-    "link": "https://mcivienna.org/#!/blog/2023/09/28/catechismo_2023_2024/",
-    "first_date_to_show": "2023-09-28",
-    "last_date_to_show": "2023-10-11"
-  },
-  {
-    "text": "Messaggio per il nuovo anno pastorale",
-    "link": "https://mcivienna.org/#!/blog/2023/09/28/messaggio_per_il_nuovo_anno_pastorale/",
-    "first_date_to_show": "2023-09-28",
-    "last_date_to_show": "2023-10-28"
+    "first_date_to_show": "2023-11-14",
+    "last_date_to_show": "2023-11-19"
   }
 ]
