33ed200b41e21494ae4699f9af79ce2240291a53
missione-italiana-vienna <missioneitalianavienna@gmail.com>
2023-11-28 10:38:12 +0100

    added couse for the Confirmation for adults

--- home/popups.js
+++ home/popups.js
@@ -4,5 +4,47 @@
     "link": "https://mcivienna.org/#!/calendario/",
     "first_date_to_show": "2023-11-26",
     "last_date_to_show": "2023-11-30"
+  },
+  {
+    "text": "Agape della comunità con festa di Natale: 17 Dicembre",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2023-12-03",
+    "last_date_to_show": "2023-12-14"
+  },
+  {
+    "text": "Prossima agape della comunità: 21 Gennaio",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2024-01-07",
+    "last_date_to_show": "2024-01-21"
+  },
+  {
+    "text": "Prossima agape della comunità: 10 Marzo",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2024-02-25",
+    "last_date_to_show": "2024-03-10"
+  },
+  {
+    "text": "Prossima agape della comunità: 21 Aprile",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2024-04-07",
+    "last_date_to_show": "2024-04-21"
+  },
+  {
+    "text": "Prossima agape della comunità: 20 Maggio (Lunedì di Pentecoste) nel giardino del convento",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2024-05-06",
+    "last_date_to_show": "2024-05-20"
+  },
+  {
+    "text": "Prossima agape della comunità: 26 Maggio",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2024-05-21",
+    "last_date_to_show": "2024-05-26"
+  },
+  {
+    "text": "Iscrizione al corso per la Cresima per gli adulti",
+    "link": "https://docs.google.com/forms/d/e/1FAIpQLSfH-f3sjxPBGHH5AZ2c9J4PpH1z7pRlp8Rcy04I0KwlltWfgg/viewform",
+    "first_date_to_show": "2023-11-28",
+    "last_date_to_show": "2024-01-31"
   }
 ]
