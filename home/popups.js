bdf101a706cc22e4052fcef1644b2bd19b435b39
Missione Italiana Vienna <43502870+missione-italiana-vienna@users.noreply.github.com>
2024-09-24 15:40:13 +0200

    Added Domenica dei Popoli

--- home/popups.js
+++ home/popups.js
@@ -1,15 +1,15 @@
 [
   {
-    "text": "Cambiamenti alle linee dei tram 33, 43 e 44 tra il 16 Aprile e il 21 Settembre",
-    "link": "https://mcivienna.org/#!/contatti/",
-    "first_date_to_show": "2024-03-11",
-    "last_date_to_show": "2024-09-21"
-  },
-  {
     "text": "Prossima agape della comunità: 26 Maggio",
     "link": "https://mcivienna.org/#!/calendario/",
     "first_date_to_show": "2024-05-21",
     "last_date_to_show": "2024-05-26"
+  },
+  {
+    "text": "Domenica dei popoli: 29 Settembre",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2024-09-24",
+    "last_date_to_show": "2024-09-29"
   },
   {
     "text": "Iscrizione al catechismo per l'anno pastorale 2024-2025",
