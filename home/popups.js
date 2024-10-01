74e48fcc2bc0ed7c5b6b0b980fcfd575b8cf2a79
Missione Italiana Vienna <43502870+missione-italiana-vienna@users.noreply.github.com>
2024-10-01 06:32:07 +0200

    Added event for October 6th

--- home/popups.js
+++ home/popups.js
@@ -11,6 +11,12 @@
     "first_date_to_show": "2024-09-24",
     "last_date_to_show": "2024-09-29"
   },
+    {
+    "text": "Domenica 6 Ottobre: S. Messa alle ore 11 (invece che 11:30) insieme con la comunità austriaca",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2024-10-01",
+    "last_date_to_show": "2024-10-06"
+  },
   {
     "text": "Iscrizione al catechismo per l'anno pastorale 2024-2025",
     "link": "https://mcivienna.org/#!/blog/2024/09/12/catechismo_2024_2025",
