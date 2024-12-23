ebd53c1074e17c7dd2e5b087da4ccf9b0e7d60b7
Missione Italiana Vienna <43502870+missione-italiana-vienna@users.noreply.github.com>
2024-12-23 14:24:10 +0100

    Added a popup with the message for Christmas 2024

--- home/popups.js
+++ home/popups.js
@@ -1,26 +1,8 @@
 [
   {
-    "text": "Prossima agape della comunità: 26 Maggio",
-    "link": "https://mcivienna.org/#!/calendario/",
-    "first_date_to_show": "2024-05-21",
-    "last_date_to_show": "2024-05-26"
-  },
-  {
-    "text": "Domenica dei popoli: 29 Settembre",
-    "link": "https://mcivienna.org/#!/calendario/",
-    "first_date_to_show": "2024-09-24",
-    "last_date_to_show": "2024-09-29"
-  },
-    {
-    "text": "Domenica 6 Ottobre: S. Messa alle ore 11 (invece che 11:30) insieme con la comunità austriaca",
-    "link": "https://mcivienna.org/#!/calendario/",
-    "first_date_to_show": "2024-10-01",
-    "last_date_to_show": "2024-10-06"
-  },
-  {
-    "text": "Iscrizione al catechismo per l'anno pastorale 2024-2025",
-    "link": "https://mcivienna.org/#!/blog/2024/09/12/catechismo_2024_2025",
-    "first_date_to_show": "2024-09-12",
-    "last_date_to_show": "2024-10-10"
+    "text": "Messaggio per Natale 2024 e per il nuovo anno",
+    "link": "https://mcivienna.org/#!/blog/2024/12/23/messaggio_per_natale_2024/",
+    "first_date_to_show": "2024-12-23",
+    "last_date_to_show": "2025-01-07"
   }
 ]
