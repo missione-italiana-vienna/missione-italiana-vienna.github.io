35fa68f641775e65cbdba19b677c67d07bacc1df
Missione Italiana Vienna <43502870+missione-italiana-vienna@users.noreply.github.com>
2025-02-14 18:28:36 +0100

    Added next agape

--- home/popups.js
+++ home/popups.js
@@ -1,8 +1,8 @@
 [
   {
-    "text": "Messaggio per Natale 2024 e per il nuovo anno",
-    "link": "https://mcivienna.org/#!/blog/2024/12/23/messaggio_per_natale_2024/",
-    "first_date_to_show": "2024-12-23",
-    "last_date_to_show": "2025-01-07"
+    "text": "Prossima agape della comunità: Domenica 2 Marzo",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2025-02-14",
+    "last_date_to_show": "2025-03-02"
   }
 ]
