55d2aeb50a340c29a7a242900d4409adff12ec99
Missione Italiana Vienna <43502870+missione-italiana-vienna@users.noreply.github.com>
2025-03-01 08:39:02 +0100

    Updated popup with info about mercoledì delle ceneri

--- home/popups.js
+++ home/popups.js
@@ -6,7 +6,7 @@
     "last_date_to_show": "2025-03-02"
   },
   {
-    "text": "Mercoledì delle ceneri: mercoledì 5 Marzo alle ore 19:30",
+    "text": "Celebrazione del mercoledì delle ceneri: mercoledì 5 Marzo alle ore 19:30",
     "link": "https://mcivienna.org/#!/calendario/",
     "first_date_to_show": "2025-03-01",
     "last_date_to_show": "2025-03-05"
