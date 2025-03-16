56e7dd4c253dce0c4688867e34162f4da18e3ac0
Missione Italiana Vienna <43502870+missione-italiana-vienna@users.noreply.github.com>
2025-03-16 21:12:01 +0100

    Added via crucis of 21.03.2025

--- home/popups.js
+++ home/popups.js
@@ -6,9 +6,9 @@
     "last_date_to_show": "2025-03-02"
   },
   {
-    "text": "Celebrazione del mercoledì delle ceneri: mercoledì 5 Marzo alle ore 19:30",
+    "text": "Via Crucis venerdì 21 Marzo",
     "link": "https://mcivienna.org/#!/calendario/",
-    "first_date_to_show": "2025-03-01",
-    "last_date_to_show": "2025-03-05"
+    "first_date_to_show": "2025-03-16",
+    "last_date_to_show": "2025-03-21"
   }
 ]
