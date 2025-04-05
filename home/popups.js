61a2662be8ec74fd7abdadba7b8e569259bfa4f6
Missione Italiana Vienna <43502870+missione-italiana-vienna@users.noreply.github.com>
2025-04-05 14:17:22 +0200

    Added changes because of the marathon 2025

--- home/popups.js
+++ home/popups.js
@@ -6,9 +6,15 @@
     "last_date_to_show": "2025-03-02"
   },
   {
-    "text": "Via Crucis venerdì 21 Marzo",
+    "text": "Domenica 6 Aprile: solo catechismo per il gruppo della Prima Comunione 2025",
     "link": "https://mcivienna.org/#!/calendario/",
-    "first_date_to_show": "2025-03-16",
-    "last_date_to_show": "2025-03-21"
+    "first_date_to_show": "2025-04-05",
+    "last_date_to_show": "2025-04-06"
+  },
+  {
+    "text": "Domenica 6 Aprile: attenzione ai blocchi alla circolazione a causa della maratona di Vienna",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2025-04-05",
+    "last_date_to_show": "2025-04-06"
   }
 ]
