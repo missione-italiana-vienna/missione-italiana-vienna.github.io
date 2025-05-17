26b5648914d8f996299167497acf6806dc06a63a
Missione Italiana Vienna <43502870+missione-italiana-vienna@users.noreply.github.com>
2025-05-17 11:49:22 +0200

    Added info about the cancellation of catechismo

--- home/popups.js
+++ home/popups.js
@@ -10,5 +10,11 @@
     "link": "https://mcivienna.org/#!/calendario/",
     "first_date_to_show": "2025-05-16",
     "last_date_to_show": "2025-05-23"
+  },
+  {
+    "text": "Domenica 18 Maggio: per motivi organizzativi gli incontri di catechismo di questo giorno sono annullati",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2025-05-17",
+    "last_date_to_show": "2025-05-18"
   }
 ]
