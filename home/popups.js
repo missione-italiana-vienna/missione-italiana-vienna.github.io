48902de764a4911735af10802ffac16424d9cafc
Missione Italiana Vienna <43502870+missione-italiana-vienna@users.noreply.github.com>
2025-06-08 07:56:48 +0200

    Added new events, removed old ones

--- home/popups.js
+++ home/popups.js
@@ -1,11 +1,5 @@
 [
   {
-    "text": "Prossima agape della comunità: Domenica 2 Marzo",
-    "link": "https://mcivienna.org/#!/calendario/",
-    "first_date_to_show": "2025-02-14",
-    "last_date_to_show": "2025-03-02"
-  },
-  {
     "text": "Messaggio per Pasqua e Ascensione 2025",
     "link": "https://mcivienna.org/#!/blog/2025/05/27/messaggio_per_pasqua_e_ascensione_2025/",
     "first_date_to_show": "2025-05-27",
@@ -22,5 +16,11 @@
     "link": "https://mcivienna.org/#!/calendario/",
     "first_date_to_show": "2025-06-09",
     "last_date_to_show": "2025-06-15"
+  },
+  {
+    "text": "Solennità del Corpus Domini  (19 Giugno): Celebrazioni solo in tedesco assieme alle alle parrocchie di Alser Vorstadt, Breitenfeld e Maria Treu.",
+    "link": "https://mcivienna.org/#!/calendario/",
+    "first_date_to_show": "2025-06-10",
+    "last_date_to_show": "2025-06-19"
   }
 ]
