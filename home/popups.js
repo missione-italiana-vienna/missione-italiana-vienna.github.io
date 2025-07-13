f49328334f9b7f11b157332a8c442b837f3d3b61
Missione Italiana Vienna <43502870+missione-italiana-vienna@users.noreply.github.com>
2025-07-13 20:16:45 +0200

    Removed old links, added link to the renovation works

--- home/popups.js
+++ home/popups.js
@@ -1,26 +1,8 @@
 [
   {
-    "text": "Messaggio per Pasqua e Ascensione 2025",
-    "link": "https://mcivienna.org/#!/blog/2025/05/27/messaggio_per_pasqua_e_ascensione_2025/",
-    "first_date_to_show": "2025-05-27",
-    "last_date_to_show": "2025-06-30"
-  },
-  {
-    "text": "Agape della Comunità Italiana: Lunedì 9 Giugno (Lunedì di Pentecoste)",
-    "link": "https://mcivienna.org/#!/calendario/",
-    "first_date_to_show": "2025-05-27",
-    "last_date_to_show": "2025-06-09"
-  },
-  {
-    "text": "Solennità della Santissima Trinità (15 Giugno): Unica S. Messa in Tedesco e Italiano alle ore 11:00, a seguire festa della parrocchia",
-    "link": "https://mcivienna.org/#!/calendario/",
-    "first_date_to_show": "2025-06-09",
-    "last_date_to_show": "2025-06-15"
-  },
-  {
-    "text": "Solennità del Corpus Domini  (19 Giugno): Celebrazioni solo in tedesco assieme alle alle parrocchie di Alser Vorstadt, Breitenfeld e Maria Treu.",
-    "link": "https://mcivienna.org/#!/calendario/",
-    "first_date_to_show": "2025-06-10",
-    "last_date_to_show": "2025-06-19"
+    "text": "Ristrutturazione della chiesa di Alser Straße",
+    "link": "https://mcivienna.org/#!/ristrutturazione/",
+    "first_date_to_show": "2025-07-13",
+    "last_date_to_show": "2026-12-31"
   }
 ]
