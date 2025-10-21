84571dd7fd4ec10992b8a28af98e89a237e8dbac
Missione Italiana Vienna <43502870+missione-italiana-vienna@users.noreply.github.com>
2025-10-21 09:35:55 +0200

    Canceled an event

--- home/popups.js
+++ home/popups.js
@@ -5,16 +5,16 @@
     "first_date_to_show": "2025-07-13",
     "last_date_to_show": "2026-12-31"
   },
-   {
-    "text": "Messaggio per il nuovo anno pastorale",
-    "link": "https://mcivienna.org/#!/blog/2025/09/17/messaggio_per_il_nuovo_anno_pastorale",
-    "first_date_to_show": "2025-09-17",
-    "last_date_to_show": "2025-10-31"
-  },
   {
     "text": "Congratulazioni al nuovo arcivescovo nominato",
     "link": "https://mcivienna.org/#!/blog/2025/10/18/nomina_del_nuovo_arcivescovo_di_vienna",
     "first_date_to_show": "2025-10-18",
     "last_date_to_show": "2025-10-26"
-  }
+  },
+  {
+    "text": "Lectio Divina per giovani e giovani adulti di giovedì 23 Ottobre: cancellata",
+    "link": "https://mcivienna.org/#!/calendario",
+    "first_date_to_show": "2025-10-21",
+    "last_date_to_show": "2025-10-23"
+  },
 ]
